1ef5b7a10ca9b7b52a3cf726d2ba68c03a7c23a9
Romário Ferreira <romarioferreirarezende@hotmail.com>
2023-05-16 21:46:41 -0300

    :sparkles: feat: adicionado imagens

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,18 +1,24 @@
 import Input from 'components/Input';
 import './styles.css';
 import { Link } from 'react-router-dom';
+import { ReactComponent as BannerImage } from 'assets/images/Banner.svg';
 
 const Login = () => {
   const inputEvent = () => {};
 
   return (
     <div className="login-container">
-      <h1 className="login-title">LOGIN</h1>
-      <Input value="Email" onChange={inputEvent} />
-      <Input value="Senha" onChange={inputEvent} />
-      <Link to="/movies">
-        <button className="btn-login">FAZER LOGIN</button>
-      </Link>
+      <div className="image-login">
+        <BannerImage />
+      </div>
+      <div className="card-login">
+        <h1 className="login-title">LOGIN</h1>
+        <Input value="Email" onChange={inputEvent} />
+        <Input value="Senha" onChange={inputEvent} />
+        <Link to="/movies">
+          <button className="btn-login">FAZER LOGIN</button>
+        </Link>
+      </div>
     </div>
   );
 };

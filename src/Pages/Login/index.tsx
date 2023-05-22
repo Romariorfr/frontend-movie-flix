4cae9d6514ef0a148f68e537e281603ced4e5ac7
Romário Ferreira <romarioferreirarezende@hotmail.com>
2023-05-21 23:04:06 -0300

    :lipstick: feat: ajuste no estilo da page de login

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -23,49 +23,45 @@ const Login = () => {
   } = useForm<FormData>();
 
   return (
-    <div className="container">
+    <form onSubmit={handleSubmit(onSubmit)} className="container">
       <div className="image-login">
         <h1>Avalie Filmes</h1>
         <p>Diga o que você achou do seu filme favorito</p>
         <BannerImage />
       </div>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="card-login">
+      <div className="card-login">
+        <h1 className="login-title">LOGIN</h1>
 
-          <h1 className="login-title">LOGIN</h1>
+        <input
+          {...register('email', {
+            required: 'Campo obrigatório',
+          })}
+          type="text"
+          className={`input-default form-control base-input ${
+            errors.password ? 'is-invalid' : ''
+          }`}
+          placeholder="Email"
+          name="email"
+        />
 
-          <input
-            {...register('email', {
-              required: 'Campo obrigatório',
-            })}
-            type="text"
-            className={`input-default form-control base-input ${
-              errors.password ? 'is-invalid' : ''
-            }`}
-            placeholder="Email"
-            name="email"
-          />
+        <input
+          {...register('password', {
+            required: 'Campo obrigatório',
+          })}
+          type="password"
+          className={`input-default form-control base-input ${
+            errors.password ? 'is-invalid' : ''
+          }`}
+          placeholder="Password"
+          name="password"
+        />
 
-          <input 
-            {...register('password', {
-              required: 'Campo obrigatório',
-            })}
-            type="password"
-            className={`input-default form-control base-input ${
-              errors.password ? 'is-invalid' : ''
-            }`}
-            placeholder="Password"
-            name="password"
-          />
-
-          <button type="submit" className="btn-login2">
-            FAZER LOGIN
-          </button>
-
-        </div>
-      </form>
-    </div>
+        <button type="submit" className="btn-login2">
+          FAZER LOGIN
+        </button>
+      </div>
+    </form>
   );
 };
 

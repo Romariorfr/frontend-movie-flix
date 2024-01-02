deebe763d8e805f57f337452c9d74df4f11d003e
Romário Ferreira <romarioferreirarezende@hotmail.com>
2024-01-01 21:41:58 -0300

    feat:added searcheable component,created link to movies

--- src/Pages/Movies/index.tsx
+++ src/Pages/Movies/index.tsx
@@ -1,18 +1,63 @@
 import { Link } from 'react-router-dom';
 import './styles.css';
+import { Controller, useForm } from 'react-hook-form';
+import ReactSelect from 'react-select';
+import { ReactComponent as Movie1 } from 'Assets/images/image1.svg';
 
 const Movies = () => {
+  const { control } = useForm();
+
+  const options = [
+    'Ação',
+    'Aventura',
+    'Comédia',
+    'Drama',
+    'Ficção Científica',
+    'Romance',
+    'Suspense',
+    'Terror',
+  ];
+
   return (
     <div className="movie-container">
-      <h1>Tela de listagem de filmes</h1>
+      <div className="genre-filter-container">
+        <Controller
+          name="category"
+          control={control}
+          render={({ field }) => (
+            <ReactSelect
+              {...field}
+              options={options.map((category) => ({
+                value: category,
+                label: category,
+              }))}
+              placeholder="Categorias"
+              isClearable
+              isSearchable
+              classNamePrefix={'category-filter-select'}
+            />
+          )}
+        />
+      </div>
+
       <Link to="/movies/1">
-        <h3>Acessar/movies/1</h3>
+        <div className="movie-card-container">
+          <Movie1 />
+          <h3>O Retorno do Rei</h3>
+          <h5>2013</h5>
+          <h6>O olho do inimigo está se movendo.</h6>
+        </div>
       </Link>
       <Link to="/movies/2">
-        <h3>Acessar/movies/2</h3>
+        <div className="movie-card-container">
+          <Movie1 />
+          <h3>O Retorno do Rei</h3>
+          <h5>2013</h5>
+          <h6>O olho do inimigo está se movendo.</h6>
+        </div>
       </Link>
     </div>
   );
 };
 
-export default Movies; 
+export default Movies;
